3953a0508b2559ce3e2c70c5edbe3f12e87e4922
Louisa HAFSI <lhafsi@e1r8p7.42.fr>
2021-12-22 15:50:31 +0100

    structure generale du projet

---
+++ ft_conversions.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_conversions.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lhafsi <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/20 20:00:40 by lhafsi            #+#    #+#             */
+/*   Updated: 2021/12/20 20:09:06 by lhafsi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+cspdiuxX%
+
+• %c imprime un seul caractère.
+• %s imprime une chaîne de caractères.
+• %p L’argument de pointeur void * est imprimé en hexadécimal.
+• %d imprime un nombre décimal (base 10).
+• %i imprime un entier en base 10.
+• %u imprime un nombre décimal non signé (base 10).
+• %x imprime un nombre en hexadécimal (base 16).
+• %% imprime un signe de pourcentage.
